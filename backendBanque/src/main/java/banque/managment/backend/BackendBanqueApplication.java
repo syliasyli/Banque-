4c13dbbe8be0b85f761864090ab5347e4e855f9f
samah sylia <samahsylia2511@gmail.com>
2022-10-25 19:18:46 +0200

    First commit

---
+++ backendBanque/src/main/java/banque/managment/backend/BackendBanqueApplication.java
@@ -0,0 +1,13 @@
+package banque.managment.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBanqueApplication.class, args);
+	}
+
+}
---
+++ backendBanque/src/test/java/banque/managment/backend/BackendBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package banque.managment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
